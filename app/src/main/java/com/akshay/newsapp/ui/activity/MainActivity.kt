dc5a95753844880a0adc80255b49eecd6c29f33e
maru01 <marumeyuu@adminnoMacBook-puro-7.local>
2018-09-13 12:19:16 +0900

    Initial commit.

---
+++ app/src/main/java/com/akshay/newsapp/ui/activity/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.akshay.newsapp.ui.activity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.akshay.newsapp.R
+import com.akshay.newsapp.adapter.NewsArticlesAdapter
+import com.akshay.newsapp.ui.NewsArticleViewModel
+import com.akshay.newsapp.utils.getViewModel
+import com.akshay.newsapp.utils.load
+import com.akshay.newsapp.utils.observe
+import com.akshay.newsapp.utils.toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.empty_layout.*
+import kotlinx.android.synthetic.main.progress_layout.*
+
+/**
+ * The Main or Starting Activity.
+ *
+ * @author Akshay Chordiya
+ * @since 5/23/2017.
+ */
+class MainActivity : AppCompatActivity() {
+
+    private val newsArticleViewModel by lazy { getViewModel<NewsArticleViewModel>() }
+
+    /**
+     * Starting point of the activity
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Setting up RecyclerView and adapter
+        news_list.setEmptyView(empty_view)
+        news_list.setProgressView(progress_view)
+
+        val adapter = NewsArticlesAdapter {
+            toast("Clicked on item")
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(it.url)))
+        }
+        news_list.adapter = adapter
+        news_list.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+
+        // Observing for data change
+        newsArticleViewModel.getNewsArticles().observe(this) {
+            it.load(news_list) {
+                // Update the UI as the data has changed
+                it?.let { adapter.replaceItems(it) }
+            }
+        }
+
+    }
+}
