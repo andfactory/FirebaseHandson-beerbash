ed7cb631d8557009dde7d66ab12e503b107e94b6
maru01 <marumeyuu@adminnoMacBook-puro-7.local>
2018-09-18 22:51:27 +0900

    Refactor

---
+++ start/src/main/java/com/akshay/newsapp/ui/top/TopViewModel.kt
@@ -0,0 +1,40 @@
+package com.akshay.newsapp.ui.top
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.akshay.newsapp.api.NewsSourceService
+import com.akshay.newsapp.db.DatabaseCreator
+import com.akshay.newsapp.model.NewsArticles
+import com.akshay.newsapp.model.network.Resource
+import com.akshay.newsapp.repo.NewsRepository
+
+/**
+ * A container for [NewsArticles] related data to show on the UI.
+ */
+class TopViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var newsArticles: LiveData<Resource<List<NewsArticles>?>>
+    private val newsRepository: NewsRepository = NewsRepository(
+            DatabaseCreator.database(application).newsArticlesDao(),
+            NewsSourceService.getNewsSourceService()
+    )
+
+    init {
+        // TODO: Inject repository using DI.
+        newsArticles = newsRepository.getNewsArticles()
+    }
+
+    /**
+     * Return news articles to observe on the UI.
+     */
+    fun getNewsArticles() = newsArticles
+
+    fun favorite(articles: NewsArticles): Int {
+        return newsRepository.favorite(articles)
+    }
+
+    fun unfavorite(articles: NewsArticles): Int {
+        return newsRepository.unfavorite(articles)
+    }
+}
